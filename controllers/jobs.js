781441aa1a2064c1461cdb1e042fa585f1f4b4b2
sergioluis24 <sergioluisl2324@gmail..com>
2026-03-24 10:20:47 -0400

    Fix:Awaits and invert params (request,response)

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,7 +1,8 @@
 import { JobModel } from "../models/job";
+import { DEFAULTS } from "../config.js";
 
 export class JobCrontroller {
-  static async addJob(response, request) {
+  static async addJob(request, response) {
     const { titulo, empresa, ubicacion, descripcion, data, content } =
       request.body;
     const newJob = await JobModel.addJob({
@@ -15,7 +16,7 @@ export class JobCrontroller {
     return response.status(201).json(newJob);
   }
 
-  static async getAll(response, request) {
+  static async getAll(request, response) {
     const {
       technology = "",
       modalidad = "",
@@ -41,31 +42,31 @@ export class JobCrontroller {
       offset: DEFAULTS.offset,
     });
   }
-  static async getById(response, request) {
+  static async getById(request, response) {
     const { id } = request.params;
-    const jobUnique = JobModel.getById({ id });
+    const jobUnique = await JobModel.getById({ id });
     return response.json(jobUnique);
   }
-  static async updateJob(response, reuest) {
+  static async updateJob(request, response) {
     const { id } = request.params;
-    const jobIndex = JobModel.verifyJob({ id });
+    const jobIndex = await JobModel.verifyJob({ id });
     if (jobIndex === -1) {
       return response.status(404).json({ error: "Job not found" });
     }
     const update = request.body;
 
-    const updatedJob = JobModel.updateJob({ id, update });
+    const updatedJob = await JobModel.updateJob({ id, updatedJob: update });
 
     return response.status(200).json(updatedJob);
   }
-  static async deleteJob(response, request) {
+  static async deleteJob(request, response) {
     const { id } = request.params;
 
-    const jobIndex = JobModel.verifyJob({ id });
+    const jobIndex = await JobModel.verifyJob({ id });
     if (jobIndex === -1) {
       return response.status(404).json({ error: "Job not found" });
     }
-    const deletedJob = JobModel.deleteJob({ id });
+    const deletedJob = await JobModel.deleteJob({ id });
     return response.status(200).json(deletedJob);
   }
 }
